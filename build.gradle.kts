7cb488cc681ac1f6734b21fb1e3c38c2455588e5
nastja123456789 <60920907+nastja123456789@users.noreply.github.com>
2025-01-22 21:21:08 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,117 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization") version "1.9.22"
+    id("kotlin-kapt")
 }
 
+android {
+    namespace = "com.example.project1729"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.example.project1729"
+        minSdk = 27
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            abiFilters
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+
+    buildFeatures{
+        viewBinding = true
+//        dataBinding = true
+    }
+
+    aaptOptions {
+        noCompress += "tflite"
+    }
+
+
+}
+val koinVersion = "3.2.2"
+val coroutineVersion = "1.7.1"
+val roomVersion = "2.6.1"
+
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
+    testImplementation("junit:junit:4.13.2")
+    implementation ("com.google.code.gson:gson:2.10")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+//    // Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.5.3")
+//
+//    // Koin
+    implementation ("io.insert-koin:koin-android:$koinVersion")
+    implementation ("io.insert-koin:koin-core:$koinVersion")
+
+//    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutineVersion")
+//
+//    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+//
+//    // Serialization
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+//    //Room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+//
+//    //Graphics
+    implementation ("com.diogobernardino:williamchart:3.10.1")
+
+//    implementation("androidx.core:core-ktx:1.15.0")
+//    implementation("androidx.appcompat:appcompat:1.7.0")
+//    implementation("com.google.android.material:material:1.12.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation ("com.arthenica:ffmpeg-kit-full:4.5.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    //implementation("com.microsoft.onnxruntime:onnxruntime:1.20.0")
+
+//    implementation(libs.androidx.core.ktx)
+        implementation("androidx.core:core-ktx:1.15.0")
+
+    implementation("org.pytorch:pytorch_android_lite:1.13.0") // Or latest
+
+
+}
